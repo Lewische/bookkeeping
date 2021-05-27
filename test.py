50ca39afc3c73ac790b48da4104b7c048ad2f55e
Lewis Che <car1122334@gmail.com>
2021-05-27 17:27:51 +0800

    new version

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
+import os 
 products = []
-with open('bookeeping.csv', 'r', encoding = 'utf - 8') as f:
-	for line in f:
-		if line in 'items, price\n':
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
+if os.path.isfile('bookeeping.csv'):
+	with open('bookeeping.csv', 'r', encoding = 'utf-8') as f:
+		for line in f:
+			if line in 'items, price\n':
+				continue
+				name, price = line.strip().split(',')
+				products.append([name, price])
 while True:
    goods = input('請輸入商品名稱: ')
    if goods == 'q':
@@ -14,7 +15,7 @@ while True:
    products.append([goods, price])
 for p in products:
 	print('已記錄品項', p[0],'價格', p[1], '元')
-with open('bookeeping.csv', 'w', encoding = 'utf - 8') as f:
+with open('bookeeping.csv', 'w', encoding = 'utf-8') as f:
 	f.write('items, price\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n')
